b31954a23ccfa6a493f69a858f86903fc8929903
lucas <lucasscpereira14@gmail.com>
2025-10-04 17:15:42 -0400

    add do 1.2

--- tp1/Processos Leitores × Escritores/1.1-ambos_com_prioridade.c
+++ tp1/Processos Leitores × Escritores/1.1-ambos_com_prioridade.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <time.h>
 
-//NOTA: structs necessarias por causa da maneira como o pthread_create aceita apenas um ponteiro void como argumento
 // estrutura para a variável compartilhada
 typedef struct {
     int quantidade;
@@ -19,14 +18,15 @@ typedef struct {
 // estrutura para passar argumentos para as threads de consulta
 typedef struct {
     int id;
-    int num_leituras; 
 } ConsultaArgs;
 
 EstoqueProduto estoque_compartilhado;
-pthread_mutex_t mutex_venda; // mutex usado apenas pelas vendas
+pthread_mutex_t mutex_venda_consulta; // mutex usado apenas pelas vendas e pelas consultas
 
 void *realizar_venda(void *arg) {
     VendaArgs *args = (VendaArgs *)arg;
+
+    usleep((rand() % 100000) + 10000); //pequena pausa para embaralhar a ordem de execução das threads
 
     int id = args->id;
     int total_vendas = args->total_a_vender;
@@ -35,23 +35,23 @@ void *realizar_venda(void *arg) {
 
     // tenta adquirir o lock
     printf("[VENDA %d]: Aguardando para entrar na região crítica...\n", id);
-    pthread_mutex_lock(&mutex_venda);
+    pthread_mutex_lock(&mutex_venda_consulta);
 
     // inicio da região crítica
     printf("\n[VENDA %d]: ENTROU NA REGIÃO CRÍTICA. O estoque neste momento é: %d\n", id, estoque_compartilhado.quantidade);
     
     if (estoque_compartilhado.quantidade >= total_vendas) {
-        printf("[VENDA %d]: Estoque suficiente. Processando a venda...\n", id);
-        printf("[VENDA %d]: QUANTIDADE APÓS A VENDA: %d\n", id, estoque_compartilhado.quantidade - total_vendas);
+        printf("    [VENDA %d]: Estoque suficiente. Processando a venda...\n", id);
+        printf("    [VENDA %d]: QUANTIDADE APÓS A VENDA: %d\n", id, estoque_compartilhado.quantidade - total_vendas);
 
         // a thread dorme depois de verificar o estoque para simular um atraso na venda
         // as threads de consulta que executarem agora vãp ler um valor sujo
-        printf("[VENDA %d]: DORMINDO por 2 segundos ANTES de efetivar a baixa no estoque. Leitores podem ver valores antigos agora!\n", id);
+        printf("    [VENDA %d]: DORMINDO por 2 segundos ANTES de efetivar a baixa no estoque. Leitores poderiam ver valores antigos agora se não tivesse o mutex!\n", id);
         sleep(1); 
 
         estoque_compartilhado.quantidade -= total_vendas; 
         
-        printf("\n[VENDA %d]: Venda de %d itens CONCLUÍDA. Estoque atualizado para: %d\n", id, total_vendas, estoque_compartilhado.quantidade);
+        printf("\n    [VENDA %d]: Venda de %d itens CONCLUÍDA. Estoque atualizado para: %d\n", id, total_vendas, estoque_compartilhado.quantidade);
         
     } else {
         printf("[VENDA %d]: FALHA. Estoque inicial (%d) insuficiente para processar %d vendas.\n", id, estoque_compartilhado.quantidade, total_vendas);
@@ -59,7 +59,7 @@ void *realizar_venda(void *arg) {
     
     printf("[VENDA %d]: SAINDO da sua região crítica.\n\n", id);
     //  fim da região crítica
-    pthread_mutex_unlock(&mutex_venda);
+    pthread_mutex_unlock(&mutex_venda_consulta);
 
     free(arg);
     pthread_exit(NULL);
@@ -67,19 +67,29 @@ void *realizar_venda(void *arg) {
 
 void *consultar_estoque(void *arg) {
     ConsultaArgs *args = (ConsultaArgs *)arg;
+
+    usleep((rand() % 100000) + 10000); //pequena pausa para embaralhar a ordem de execução das threads
+
     int id = args->id;
-    int num_leituras = args->num_leituras;
     int estoque_lido;
 
-    sleep(2); 
+    printf("[CONSULTA %d]: Iniciada. Tentará consultar a quantidade disponível no estoque.\n", id);
 
-    printf("[CONSULTA %d]: Iniciada. Fará %d leituras para monitorar o estoque ao longo da simulaçãp.\n", id, num_leituras);
+    
+    // tenta adquirir o lock
+    printf("[CONSULTA %d]: Aguardando para entrar na região crítica...\n", id);
+    pthread_mutex_lock(&mutex_venda_consulta);
 
-    for (int i = 0; i < num_leituras; i++) {
-        estoque_lido = estoque_compartilhado.quantidade;
-        printf("    [CONSULTA %d]: Estoque lido: %d\n", id, estoque_lido);
-        sleep(1);
-    }
+    // inicio da região crítica
+    printf("\n[CONSULTA %d]: ENTROU NA REGIÃO CRÍTICA.\n", id);
+    printf("    [CONSULTA %d]: Lendo o estoque...\n", id);
+    estoque_lido = estoque_compartilhado.quantidade;
+    sleep(1); //simula o tempo de leitura
+    printf("    [CONSULTA %d]: Estoque lido: %d\n", id, estoque_lido);
+    printf("[CONSULTA %d]: SAINDO da sua região crítica.\n\n", id);
+    // fim da região crítica
+
+    pthread_mutex_unlock(&mutex_venda_consulta);
 
     printf("[CONSULTA %d]: Finalizada.\n", id);
     free(arg);
@@ -106,42 +116,42 @@ int main() {
     scanf("%d", &total_vendas_por_thread);
 
     int estoque_inicial = estoque_compartilhado.quantidade;
-    int leituras_por_thread = num_vendas;
+    int leituras_por_thread = num_vendas/2; 
 
-    pthread_mutex_init(&mutex_venda, NULL);
+    pthread_mutex_init(&mutex_venda_consulta, NULL);
     srand(time(NULL));
 
     pthread_t threads[num_consultas + num_vendas];
     printf("\n--- Iniciando simulação ---\n");
     printf("Estoque inicial: %d\n\n", estoque_compartilhado.quantidade);
 
-    // criando as threads de consulta (leitores SEM mutex)
-   for (int i = 0; i < num_consultas; i++) {
-        ConsultaArgs *args = malloc(sizeof(ConsultaArgs));
-        args->id = i + 1;
-        args->num_leituras = leituras_por_thread;
-        pthread_create(&threads[i], NULL, consultar_estoque, (void *)args);
-    }
-
-    // criando as threads de venda (escritores COM mutex)
-    for (int i = 0; i < num_vendas; i++) {
-        VendaArgs *args = malloc(sizeof(VendaArgs));
-        args->id = i + 1;
-        args->total_a_vender = total_vendas_por_thread;
-        pthread_create(&threads[num_consultas + i], NULL, realizar_venda, (void *)args);
+    for (int i = 0; i < (num_consultas + num_vendas); i++) { // só um laço para que eles conseguirem se intercalar no mutex
+        int j = 0;
+        int k = 0;
+        if (j < num_consultas) {
+            ConsultaArgs *args = malloc(sizeof(ConsultaArgs));
+            args->id = i+1;
+            pthread_create(&threads[i], NULL, consultar_estoque, (void *)args);
+        }
+        if (k < num_vendas) {
+            VendaArgs *args = malloc(sizeof(VendaArgs));
+            args->id = i+1;
+            args->total_a_vender = total_vendas_por_thread;
+            pthread_create(&threads[i], NULL, realizar_venda, (void *)args);
+        }
     }
 
     for (int i = 0; i < (num_consultas + num_vendas); i++) {
         pthread_join(threads[i], NULL);
     }
 
-    pthread_mutex_destroy(&mutex_venda);
+    pthread_mutex_destroy(&mutex_venda_consulta);
 
     printf("\n--- Simulação finalizada ---\n");
     printf("Estoque inicial: %d\n", estoque_inicial);
     printf("Estoque final: %d\n", estoque_compartilhado.quantidade);
     printf("\n");
-    printf("Observe se alguma thread de consulta leu o valor errado após uma thread de venda marcar que ele (o valor) seria mudado após uma venda entrar em sua região crítica.\n");
+    printf("Observe se alguma thread de consulta leu o valor errado após uma thread de venda marcar que ele (o valor) seria mudado.\n");
  
     return 0;
 }
