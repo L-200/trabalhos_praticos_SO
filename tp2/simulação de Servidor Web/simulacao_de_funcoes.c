4f8fae29b58fb09b1dfad474d11095f6b13e06e8
Pedrinnnn-lab <macaczin231@gmail.com>
2025-12-09 11:01:57 -0400

    Update simulacao_de_funcoes.c

--- tp2/simulação de Servidor Web/simulacao_de_funcoes.c
+++ tp2/simulação de Servidor Web/simulacao_de_funcoes.c
@@ -1,25 +1,25 @@
 
-#include "simulation_functions.h"
+#include "simulacao_de_funcoes.h"
 #include <stdio.h>
 #include <unistd.h> 
 
 void processar_html(int req_id) {
     int tempo = 1; // 1 segundo
-    printf("[REQUISIÇÃO %d]: INICIO - Processando HTML (Tarefa Leve, %ds)...\n", req_id, tempo);
+    printf("[REQUISICAO %d]: INICIO - Processando HTML (Tarefa Leve, %ds)...\n", req_id, tempo);
     sleep(tempo);
-    printf("[REQUISIÇÃO %d]: FIM - HTML concluído.\n", req_id);
+    printf("[REQUISICAO %d]: FIM - HTML concluido.\n", req_id);
 }
 
 void processar_consulta(int req_id) {
     int tempo = 3; // 3 segundos
-    printf("[REQUISIÇÃO %d]: INICIO - Processando Consulta DB (Tarefa Média, %ds)...\n", req_id, tempo);
+    printf("[REQUISICAO %d]: INICIO - Processando Consulta DB (Tarefa Media, %ds)...\n", req_id, tempo);
     sleep(tempo);
-    printf("[REQUISIÇÃO %d]: FIM - Consulta DB concluída.\n", req_id);
+    printf("[REQUISICAO %d]: FIM - Consulta DB concluida.\n", req_id);
 }
 
 void processar_imagem(int req_id) {
     int tempo = 5; // 5 segundos
-    printf("[REQUISIÇÃO %d]: INICIO - Processando Imagem (Tarefa Pesada, %ds)...\n", req_id, tempo);
+    printf("[REQUISICAO %d]: INICIO - Processando Imagem (Tarefa Pesada, %ds)...\n", req_id, tempo);
     sleep(tempo);
-    printf("[REQUISIÇÃO %d]: FIM - Imagem concluída.\n", req_id);
+    printf("[REQUISICAO %d]: FIM - Imagem concluida.\n", req_id);
 }
