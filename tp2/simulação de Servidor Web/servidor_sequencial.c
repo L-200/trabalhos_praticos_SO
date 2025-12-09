fab3e56aabb7f1d23357858d75a58cc7e704ebae
Pedrinnnn-lab <macaczin231@gmail.com>
2025-12-09 11:01:19 -0400

    Update servidor_sequencial.c
    correção de erros no código

--- tp2/simulação de Servidor Web/servidor_sequencial.c
+++ tp2/simulação de Servidor Web/servidor_sequencial.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include "simulação_de_funcoes.h" // Inclui as funções de simulação
+#include "simulacao_de_funcoes.h" // Inclui as funções de simulação
 
 int main() {
     // Variáveis de medição de tempo
@@ -11,17 +11,17 @@ int main() {
     int num_clientes; // Variável para armazenar a entrada do usuário com relação à quantidade de clientes (requisições) a serem processadas
     int i; 
     
-    printf("--- SERVIDOR WEB SIMULADO: VERSÃO SEQUENCIAL ---\n");
-    printf("OBJETIVO: Demonstração do bloqueio do sistema (sem threads).\n");
+    printf("--- SERVIDOR WEB SIMULADO: VERSAO SEQUENCIAL ---\n");
+    printf("OBJETIVO: Demonstracao do bloqueio do sistema (sem threads).\n");
 
     // 1. Entrada Dinâmica do Usuário
-    printf("\n[SERVIDOR]: Digite a quantidade de clientes (requisições) a simular: ");
+    printf("\n[SERVIDOR]: Digite a quantidade de clientes (requisicoes) a simular: ");
     if (scanf("%d", &num_clientes) != 1 || num_clientes <= 0) {
-        printf("[ERRO]: Entrada inválida. Encerrando.\n");
+        printf("[ERRO]: Entrada invalida. Encerrando.\n");
         return 1;
     }
     
-    printf("\n[SERVIDOR]: Simulação iniciada para %d clientes. Cada um será atendido em série...\n", num_clientes);
+    printf("\n[SERVIDOR]: Simulacao iniciada para %d clientes. Cada um sera atendido em serie...\n", num_clientes);
     
     // 2. SIMULAÇÃO DE CARGA SEQUENCIAL COM MEDIÇÃO DE TEMPO
     inicio = clock(); // Marca o início da execução
@@ -34,7 +34,7 @@ int main() {
         // Na versão interativa final, poderemos adicionar uma escolha aleatória de função.
         
         // Chamada da função pesada que contém o sleep(5)
-        processar_imagem_pesada(i); // BLOQUEIA o programa por 5 segundos
+        processar_imagem(i); // BLOQUEIA o programa por 5 segundos
     }
 
     // 3. FINALIZAÇÃO E CÁLCULO DE TEMPO
@@ -43,7 +43,7 @@ int main() {
     tempo_total = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
 
     printf("\n--- RESULTADO SEQUENCIAL ---\n");
-    printf("Total de Requisições Atendidas: %d\n", num_clientes);
+    printf("Total de Requisicoes Atendidas: %d\n", num_clientes);
     // Tempo teórico: num_clientes * 5 segundos
     printf("Tempo TOTAL de Processamento (Bloqueado): %.2f segundos.\n", tempo_total);
     printf("-------------------------------------------\n");
